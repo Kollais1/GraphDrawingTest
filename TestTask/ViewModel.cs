72a4eb717475b5f40cb088f9c595f4bd8ddfc7de
Kollais1 <katerynachernenko7@gmail.com>
2025-03-15 19:10:01 +0200

    Delete an unused example method

--- TestTask/ViewModel.cs
+++ TestTask/ViewModel.cs
@@ -106,33 +106,6 @@ namespace TestTask
             DrawYAxisLabels();
         }
 
-        private void OnPaintSurface(SKPaintSurfaceEventArgs? a)
-        {
-            if (a == null)
-                return;
-            var e = (SKPaintSurfaceEventArgs)a;
-
-            // the the canvas and properties
-            var canvas = e.Surface.Canvas;
-
-            // make sure the canvas is blank
-            canvas.Clear(SKColors.Aqua);
-
-            // draw some text
-            using var paint = new SKPaint
-            {
-                Color = SKColors.Black,
-                IsAntialias = true,
-                Style = SKPaintStyle.Fill
-            };
-            using var font = new SKFont
-            {
-                Size = 24
-            };
-            var coord = new SKPoint(e.Info.Width / 2, (e.Info.Height + font.Size) / 2);
-            canvas.DrawText("SkiaSharp", coord, SKTextAlign.Center, font, paint);
-        }
-
         public void ZoomChanged(object? arg)
         {
             if (arg == null)
